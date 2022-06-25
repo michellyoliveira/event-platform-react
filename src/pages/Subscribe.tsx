6c6888b4c015116375e5a0f942649256bf3d7037
Michelly Oliveira <michellyconnolli@gmail.com>
2022-06-25 16:32:57 -0300

    Fixing import image subscriber page

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
 
+import imgUrl from '../../src/assets/code-mockup.png';
+
 export function Subscribe() {
 const navigate = useNavigate();
 
@@ -69,7 +71,7 @@ async function handleSubscribe(event: FormEvent) {
         </div>
       </div>
 
-      <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+      <img src={imgUrl} className="mt-10" alt="" />
     </div>
   )
 }
