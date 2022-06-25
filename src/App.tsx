4495e8b329b9c8c854ab63ae9c50abe9ab520768
Michelly Oliveira <michellyconnolli@gmail.com>
2022-06-25 15:28:17 -0300

    Sending project to repository

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { ApolloProvider } from '@apollo/client';
+import { Event } from './pages/Event';
+
+import { client } from './lib/apollo';
+import { Router } from './router';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  )
+}
+
+export default App
